9aa6f9a6871dd4a717dd7a1b869b8c95a67f8d08
TonyPC <wongtonyb@gmail.com>
2025-02-01 00:33:20 -0500

    attendanceChart & financeChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,7 @@
-import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart";
+import FinanceChart from "@/components/FinanceChart";
 
 const AdminPage = () => {
   return (
@@ -20,10 +22,14 @@ const AdminPage = () => {
             <CountChart />
           </div>
           {/* ATTENDANCE CHART */}
-          <div className="w-full lg:w-2/3 h-[450px]"></div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
         </div>
         {/* BOTTOM CHART */}
-        <div className=""></div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">r</div>
